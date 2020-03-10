cb1e0adb0107c2da84e28d7d477ec81ea15addb1
CM Student <cmstudent@uhwo-acm-10.uhwo.hawaii.edu>
2020-03-10 10:45:13 -1000

    Completed HackWich 8

--- HackWichEight/ViewController.swift
+++ HackWichEight/ViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Slider: UISlider!
+    
+    var currentValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+
+    @IBAction func GuessnumberPress(_ sender: Any){
+    
+        let message = "The value is: \(currentValue)"
+       
+        
+        let alert = UIAlertController(title:"Hello World!", message: message,  preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+    
+       present(alert, animated: true, completion: nil)
+    }
+    
+  
+    @IBAction func sliderMoved(_ sender: Any) {
+    
+        print("The value of the slider is:\(Slider.value)")
+       currentValue = lroundf(Slider.value)
+    
+    
+    }
+    
 
 
 }
